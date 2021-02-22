2bdc9f6a47876c87594ea3a9d10a1246b7b5fec0
Dakota <piercedakota26@gmail.com>
2021-02-21 19:33:29 -0600

    added question list

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,74 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type:'input',
+        message:"What is your project Title:",
+        name:'Title'
+    },
+    {
+        type: 'input',
+        message:'Write Description to your project:' ,
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message:'What are the steps required to install your project?' ,
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message:'Provide instructions for usage:' ,
+        name: 'Usage'
+    },
+    {
+        type: 'input',
+        message:'Would you like other developers to contribute to it: ' ,
+        name: 'Contributing'
+    },
+    {
+        type: 'input',
+        message:'Write a tests for your application:' ,
+        name: 'Tests'
+    },    
+    {
+        type:'input',
+        message: 'List Your Collaborator or any third part assets that require attribution: ',
+        name:'credits'
+
+    },
+    {
+        type: 'checkbox',
+        message:'Choose a license: ' ,
+        choices: [
+            'MIT',
+            'Apache',
+            'GPL',
+            'BSD',
+            'None',
+        ],
+        name: 'License',
+    },
+    {
+        type:'input',
+        message: 'What is your name?:',
+        name:'username'
+
+    },
+    {
+        type:'input',
+        message: 'What is your Github User name:',
+        name:'username'
+
+    },
+    {
+        type:'input',
+        message: 'What is your email address:',
+        name:'useremail'
+    }
+    
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
