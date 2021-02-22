e234d979aa6e5afe90518e084b78988da5b1fc07
Dakota <piercedakota26@gmail.com>
2021-02-21 20:31:05 -0600

    added inquirer npm

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const readMeOutput = './readME.md'
 
 // TODO: Create an array of questions for user input
 inquirer.prompt([
